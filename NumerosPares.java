41aee7c1c75fa8c7e24449305b97d93401756b4f
Fritz-2008 <fritzanimaciones@gmail.com>
2026-06-22 09:00:19 -0500

    Add NumerosPares class to print even numbers

---
+++ NumerosPares.java
@@ -0,0 +1,9 @@
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        System.out.println("Números pares del 1 al 100:");
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
