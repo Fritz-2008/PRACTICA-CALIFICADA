ea6fc090fca3c3c14dca9331325a84acd4da8c86
Fritz-2008 <fritzanimaciones@gmail.com>
2026-06-22 09:19:46 -0500

    Create NumeroPositivo.java

---
+++ NumeroPositivo.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class NumeroPositivo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        do {
+            System.out.print("Ingrese un número positivo: ");
+            num = sc.nextInt();
+        } while (num <= 0);
+
+        System.out.println("Número válido: " + num);
+        sc.close();
+    }
+}
