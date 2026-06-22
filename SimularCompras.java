90379a066055f495d532f1d324c21242774190f0
Fritz-2008 <fritzanimaciones@gmail.com>
2026-06-22 09:50:20 -0500

    Add SimularCompras class for purchase simulation

---
+++ SimularCompras.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class SimularCompras {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double precio, total = 0;
+
+        do {
+            System.out.print("Ingrese precio (0 para terminar): ");
+            precio = sc.nextDouble();
+            total += precio;
+        } while (precio != 0);
+
+        System.out.println("Total de compra: " + total);
+        sc.close();
+    }
+}
