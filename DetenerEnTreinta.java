9531cc470149cc0bf60bf8a152ea6789d5644151
Fritz-2008 <fritzanimaciones@gmail.com>
2026-06-22 09:59:17 -0500

    Create DetenerEnTreinta.java

---
+++ DetenerEnTreinta.java
@@ -0,0 +1,11 @@
+public class DetenerEnTreinta {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++) {
+            if (i == 30) {
+                break;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
